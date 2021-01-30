a20396d091e18a9e4fef60ccc0808ab4931b1ff0
Michael Sterzinger <69076772+idep-nter@users.noreply.github.com>
2021-01-30 14:55:02 +0100

    Update umrella_reminder.py

--- umrella_reminder.py
+++ umrella_reminder.py
@@ -5,17 +5,14 @@ def umbrella():
     Checks weather of a desired location at openweathermap.org every morning 
     at 8 AM and if a forecast is going to be rainy, it logs into an e-mail and 
     sends an e-mails to take an umbrella.
-    """
-    
+    """  
     location = 'Brno, Czech republic'
     url ='http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % \
          (location)
     response = requests.get(url)
     response.raise_for_status()
-
     weatherData = json.loads(response.text)
     emails = ['xxx']
-
     if weatherData[0]['weather'] = 'Rainy'
         smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
         smtpObj.ehlo()
@@ -25,7 +22,6 @@ def umbrella():
         for email in emails:
             sendmail = smtpObj.sendmail('xxx', email, body)
         smtpObj.quit()
-
-        
+      
 if _name_ == '_name_':
     schedule.every().day.at("08:00").do(umbrella)
